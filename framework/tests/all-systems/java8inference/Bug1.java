8bd0155c891093d7ebbfdb8def85a1017c4efda9
Suzanne Millstein <smillst@cs.washington.edu>
2018-01-08 13:16:24 -0800

    Suppress warning.

--- framework/tests/all-systems/java8inference/Bug1.java
+++ framework/tests/all-systems/java8inference/Bug1.java
@@ -6,7 +6,7 @@ import java.util.Map.Entry;
 
 @SuppressWarnings("") // Just check for crashes.
 public class Bug1<B> {
-
+    @SuppressWarnings("type.inference.not.same")
     public void method1(Map<? extends Class<? extends B>, ? extends B> map) {
         Map<Class<? extends B>, B> copy = new LinkedHashMap<>(map);
         for (Entry<? extends Class<? extends B>, B> entry : copy.entrySet()) {
