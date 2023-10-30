a465e21ce055069982f99b24155f67058ec3e952
Suzanne Millstein <smillst@cs.washington.edu>
2023-10-30 11:27:02 -0700

    Add javdoc.

--- framework/src/main/java/org/checkerframework/framework/util/typeinference8/types/Qualifier.java
+++ framework/src/main/java/org/checkerframework/framework/util/typeinference8/types/Qualifier.java
@@ -3,15 +3,28 @@ package org.checkerframework.framework.util.typeinference8.types;
 import javax.lang.model.element.AnnotationMirror;
 import org.checkerframework.framework.util.typeinference8.util.Java8InferenceContext;
 
+/** A wrapper around an {@link AnnotationMirror}. */
 public class Qualifier extends AbstractQualifier {
 
+  /** The annotation. */
   private final AnnotationMirror annotation;
 
-  Qualifier(AnnotationMirror annotation, Java8InferenceContext context) {
+  /**
+   * A wrapper around an {@link AnnotationMirror}.
+   *
+   * @param annotation the annotation
+   * @param context the context
+   */
+  protected Qualifier(AnnotationMirror annotation, Java8InferenceContext context) {
     super(annotation, context);
     this.annotation = annotation;
   }
 
+  /**
+   * Returns the annotation
+   *
+   * @return the annotation
+   */
   public AnnotationMirror getAnnotation() {
     return annotation;
   }
