2da70ddde61475f40484e7c95e26b282abc40f06
Suzanne Millstein <smillst@cs.washington.edu>
2024-03-26 12:56:05 -0700

    Suppress warning.

--- framework/tests/all-systems/Issue6421C.java
+++ framework/tests/all-systems/Issue6421C.java
@@ -3,7 +3,6 @@ import java.lang.reflect.Parameter;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
-
 @SuppressWarnings("all")
 public class Issue6421C {
   private static String[] getParameterNames(Constructor<?> constructor) {
