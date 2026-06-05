7f951cf1cc9b2cbf3d203988a1a2bfd7c8d9971b
YukiYang31 <yukyang@cs.washington.edu>
2026-06-05 15:34:33 -0700

    make last commit complete.

--- checker/src/main/java/org/checkerframework/checker/modifiability/ModifiabilityBaseChecker.java
+++ checker/src/main/java/org/checkerframework/checker/modifiability/ModifiabilityBaseChecker.java
@@ -1,5 +1,6 @@
 package org.checkerframework.checker.modifiability;
 
+import org.checkerframework.checker.modifiability.grow.GrowChecker;
 import org.checkerframework.common.basetype.BaseTypeChecker;
 
 /**
@@ -7,10 +8,9 @@ import org.checkerframework.common.basetype.BaseTypeChecker;
  *
  * <p>This class exists so {@link
  * org.checkerframework.framework.source.SourceChecker#getMessagesProperties()} finds the shared
- * {@code messages.properties} file in {@code org.checkerframework.checker.modifiability}. The Grow,
- * SeqGrow, Shrink, and Replace checkers all report diagnostics whose message keys are defined
- * there; without this common superclass, those keys would need to be duplicated in each
- * sub-checker's package.
+ * {@code messages.properties} file in {@code org.checkerframework.checker.modifiability}. The
+ * Modifiability sub-checkers all report diagnostics whose message keys are defined there; without
+ * this common superclass, those keys would need to be duplicated in each sub-checker's package.
  */
 public abstract class ModifiabilityBaseChecker extends BaseTypeChecker {
 
