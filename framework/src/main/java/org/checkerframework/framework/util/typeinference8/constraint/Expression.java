f44b6ca833d5ba259006fe6777a8f186538bff1c
Michael Ernst <mernst@cs.washington.edu>
2026-08-31 21:02:38 -0700

    Remove dead code

--- framework/src/main/java/org/checkerframework/framework/util/typeinference8/constraint/Expression.java
+++ framework/src/main/java/org/checkerframework/framework/util/typeinference8/constraint/Expression.java
@@ -95,10 +95,7 @@ public class Expression extends TypeConstraint {
     if (getT().isProper()) {
       return reduceProperType();
     } else if (TreeUtils.isStandaloneExpression(expression)) {
-      AbstractType s;
-
-      s = new ProperType(expression, context);
-
+      AbstractType s = new ProperType(expression, context);
       return new Typing(this, s, T, TypeConstraint.Kind.TYPE_COMPATIBILITY);
     }
     switch (expression.getKind()) {
@@ -359,8 +356,6 @@ public class Expression extends TypeConstraint {
       if (tPrimeNotSameAsT) {
         constraintSet.add(new Typing(this, tPrime, T, TypeConstraint.Kind.SUBTYPE));
       }
-    } else {
-      context.addLambdaParms(lambda.getParameters());
     }
 
     AbstractType R = tPrime.getFunctionTypeReturnType();
